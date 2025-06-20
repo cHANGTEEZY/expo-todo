a30e29c324a4c7c17179030e99f53c91c7938f93
sushank gurung <jagatgurung30@gmail.com>
2025-06-20 19:14:37 +0545

    completed add todo and remove todo

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,75 +1,134 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import {
+  Button,
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  useColorScheme,
+  View,
+} from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const App = () => {
+  const [enteredText, setEnteredText] = useState<string>("");
+  const [courseGoals, setCourseGoals] = useState<
+    {
+      text: string;
+      key: string;
+    }[]
+  >([]);
 
-export default function HomeScreen() {
+  const colorScheme = useColorScheme();
+
+  function goalInputHandler(textInput: string) {
+    setEnteredText(textInput);
+  }
+
+  function addGoalHandler() {
+    if (enteredText.trim()) {
+      setCourseGoals((currentGoals) => [
+        ...currentGoals,
+        { text: enteredText, key: Math.random().toString() },
+      ]);
+      setEnteredText("");
+    }
+  }
+
+  function removeGoalHandler(key: string) {
+    setCourseGoals((currentGoals) =>
+      currentGoals.filter((goal) => goal.key !== key)
+    );
+  }
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={[
+            styles.textInput,
+            {
+              color: colorScheme === "dark" ? "white" : "dark",
+            },
+          ]}
+          placeholder="Your course goal"
+          onChangeText={goalInputHandler}
+          value={enteredText}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+        <Button title="Add goal" onPress={addGoalHandler} />
+      </View>
+      <View style={styles.goalsContainer}>
+        <FlatList
+          data={courseGoals}
+          renderItem={({ item }) => (
+            <View key={item.key} style={styles.goalItems}>
+              <Text style={styles.goalText}>{item.text}</Text>
+              <Text
+                style={styles.goalCross}
+                onPress={() => removeGoalHandler(item.key)}
+              >
+                X
+              </Text>
+            </View>
+          )}
+          keyExtractor={(item) => item.key}
+        />
+      </View>
+    </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  appContainer: {
+    paddingTop: 50,
+    paddingHorizontal: 16,
+    flex: 1,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "80%",
+    marginRight: 8,
+    padding: 8,
+  },
+
+  goalsContainer: {
+    flex: 5,
+    marginBottom: 20,
+  },
+
+  goalItems: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    fontSize: 20,
+    backgroundColor: "#530acc",
+    margin: 8,
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 20,
+    borderRadius: 20,
+  },
+
+  goalText: {
+    color: "white",
+  },
+
+  goalCross: {
+    color: "white",
+    position: "absolute",
+    right: 20,
+    top: 10,
   },
 });
